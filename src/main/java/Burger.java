bfc0a4110084fb922a5be195bd062c60ce6635d0
Roman <groma_92@mail.ru>
2019-09-26 13:50:51 +0300

    help

---
+++ src/main/java/Burger.java
@@ -0,0 +1,11 @@
+public class Burger {
+    public static void burger(String[] args) {
+        long id;
+        int price;
+        String name;
+        String photo;
+        int weight;
+        int kcal;
+        boolean hot;
+    }
+}
