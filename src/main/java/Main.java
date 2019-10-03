fb7588ce5d818bce0c438971cebb432625156201
Roman <groma_92@mail.ru>
2019-10-03 08:59:58 +0300

    помощь не нужна

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        burger chefBurger = new burger();
-        chefBurger.id = 2380;
-        chefBurger.price = 144;
-        chefBurger.weight = 239;
+        Burger chefBurger = new Burger();
+        chefBurger.name = "chef";
+        chefBurger.id = 2384;
+        chefBurger.price = 129;
+        chefBurger.weight = 213;
+        chefBurger.kcal = 236;
         System.out.println(chefBurger.id);
+
+
+        Burger longerBBQ = new Burger();
+        longerBBQ.id = 1018;
+        longerBBQ.price = 50;
+        longerBBQ.weight = 107;
+        longerBBQ.kcal = 236;
+        System.out.println(longerBBQ.weight);
+
+
+        Burger chefBurgerJunior = new Burger();
+        chefBurgerJunior.id = 2962;
+        chefBurgerJunior.price = 99;
+        chefBurgerJunior.weight = 174;
+        chefBurgerJunior.kcal = 244;
+        System.out.println(chefBurgerJunior.kcal);
 
 
     }
